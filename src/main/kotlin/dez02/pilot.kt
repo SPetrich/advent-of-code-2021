c3eac99b15575de0543894cc6d985771bdf4c1fc
Sebastian Petrich <sebastian.petrich@exaring.de>
2021-12-02 14:02:06 +0100

    2. dez - simplify

--- src/main/kotlin/dez02/pilot.kt
+++ src/main/kotlin/dez02/pilot.kt
@@ -1,26 +1,17 @@
 import java.io.File
-import java.lang.Integer.MAX_VALUE
 
 fun main () {
-
-    val fwPattern = "forward "
-    val downPattern = "down "
-    val upPattern = "up "
 
     var x = 0
     var depth = 0
     File(ClassLoader.getSystemResource("dez02/input.txt").toURI()).forEachLine{
-        with(it) {
-            when {
-                startsWith(fwPattern) -> x += it.substringAfter(" ").toInt()
-                startsWith(downPattern) -> depth += it.substringAfter(" ").toInt()
-                startsWith(upPattern) -> depth -= it.substringAfter(" ").toInt()
-                else -> println("ERROR")
-            }
+        when {
+            it.startsWith("forward ") -> x += it.substringAfter(" ").toInt()
+            it.startsWith("down ") -> depth += it.substringAfter(" ").toInt()
+            it.startsWith("up ") -> depth -= it.substringAfter(" ").toInt()
+            else -> println("ERROR")
         }
     }
-    println(x)
-    println(depth)
     println(x * depth)
 
 }
